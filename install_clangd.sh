868db330e350e7ce2f17f5290b13dd9b1fd5c0be
Li Lingjie <lljbash@gmail.com>
2020-03-13 15:36:19 +0800

    fix clangd installation

--- install_clangd.sh
+++ install_clangd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 sudo add-apt-repository "deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch main"
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key\|sudo apt-key add -
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 
 sudo apt-get update
 sudo apt-get install clang clangd
