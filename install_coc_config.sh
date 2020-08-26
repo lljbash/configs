31c47479bf628c97a02a829964929a4f68c69d9b
Lingjie Li <lljbash@gmail.com>
2020-08-26 18:37:25 +0800

    ccls cache directories moved to $HOME; avoid permission issues; run install_coc_config to deploy.

--- install_coc_config.sh
+++ install_coc_config.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cp ./coc-settings.json ~/.vim/coc-settings.json
+home_double_slash=`echo $HOME | sed 's/\//\\\\\//g'`
+sed -i "s/REPLACE_WITH_HOME_CCLS_CACHE/\"$home_double_slash\/.ccls-cache\"/g" ~/.vim/coc-settings.json
 
 if [ ! -x "$(command -v ccls)" ]; then
     echo Warning: ccls is required to use ccls LSP
