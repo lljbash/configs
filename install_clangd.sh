a5e9e6860fdb5178c7c6a5c69c1354935cfa8ee0
Li Lingjie <lljbash@gmail.com>
2020-09-17 16:23:34 +0800

    install stable llvm

--- install_clangd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-sudo add-apt-repository "deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch main"
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-
-sudo apt-get update
-sudo apt-get install clang clangd
-
---
+++ install_llvm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
