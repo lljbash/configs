cc326afbdd4bef6a3ddb9bc872c02a835b492395
lljbash <lljbash@gmail.com>
2021-06-28 23:02:21 +0800

    my aliases for zinit snippet

---
+++ my_aliases.zsh
@@ -0,0 +1,14 @@
+# vim
+(( $+commands[vi] )) || alias vi='vim'
+export EDITOR=vim
+
+# cmake
+alias cconf="cmake -H. -Bbuild"
+alias cconfr="cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release"
+alias cconfd="cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug"
+alias creconf="rm -rf build; cmake -H. -Bbuild"
+alias creconfr="rm -rf build; cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release"
+alias creconfd="rm -rf build; cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug"
+alias cbuild="cmake --build build --"
+alias crebuild="cmake --build build --clean-first --"
+alias cclean="cmake --build build --target clean"
