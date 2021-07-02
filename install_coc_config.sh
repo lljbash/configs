8f5104c832677761409556c394a101af78104333
lljbash <lljbash@gmail.com>
2021-07-02 11:22:26 +0800

    fix sed error for macos

--- install_coc_config.sh
+++ install_coc_config.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-cp ./coc-settings.json ~/.vim/coc-settings.json
+vim_prefix=~/.vim
+coc_settings=coc-settings.json
+mkdir -p $vim_prefix
+cp $coc_settings $vim_prefix/$coc_settings.temp
+cd $vim_prefix
 home_double_slash=`echo $HOME | sed 's/\//\\\\\//g'`
-sed -i "s/REPLACE_WITH_HOME_CCLS_CACHE/\"$home_double_slash\/.ccls-cache\"/g" ~/.vim/coc-settings.json
+sed "s/REPLACE_WITH_HOME_CCLS_CACHE/\"$home_double_slash\/.ccls-cache\"/g" $coc_settings.temp > $coc_settings
+rm $coc_settings.temp
 
 if [ ! -x "$(command -v ccls)" ]; then
     echo Warning: ccls is required to use ccls LSP
 fi
-
