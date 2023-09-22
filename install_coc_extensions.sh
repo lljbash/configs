c50220f76064098ae3b1e768c3e598fd7696662a
Lingjie Li <lilingjie@sensetime.com>
2023-09-22 10:50:29 +0800

    coc-python -> coc-pyright; remove coc-tabnine

--- install_coc_extensions.sh
+++ install_coc_extensions.sh
@@ -19,12 +19,12 @@ npm install coc-snippets@2.4.5 --global-style --ignore-scripts --no-bin-links --
 npm install coc-highlight --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-calc --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-yank --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
-npm install coc-python --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+npm install coc-pyright --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 #npm install coc-clangd --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-pairs --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-json --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-cmake --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
-npm install coc-tabnine --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+#npm install coc-tabnine --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-fzf-preview --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 npm install coc-typos --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 
