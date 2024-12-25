5f7f17d3f91477d3790e269dd8372aad26ed2564
Lingjie <lljbash@gmail.com>
2024-12-25 15:48:21 +0800

    feat: add CopilotChat plugin

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -15,4 +15,14 @@ return {
       },
     },
   },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {},
+  },
 }
