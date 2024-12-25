402aff1acf9959017d58d40389ad52064ef81efa
Lingjie <lljbash@gmail.com>
2024-12-25 16:39:29 +0800

    feat(nvim): add keybindings for CopilotChat

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- Plugin for GitHub Copilot integration
     "zbirenbaum/copilot.lua",
     opts = {
       suggestion = {
@@ -17,12 +18,45 @@ return {
   },
 
   {
+    -- Plugin for Copilot Chat integration
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "zbirenbaum/copilot.lua" },
+      "zbirenbaum/copilot.lua",
       { "nvim-lua/plenary.nvim", branch = "master" },
+      "folke/which-key.nvim",
     },
     build = "make tiktoken",
-    opts = {},
+    config = function()
+      local cc = require("CopilotChat")
+      cc.setup {}
+      local wk = require("which-key")
+      wk.add({
+        mode = { "n", "v" },
+        { "<leader>c", group = "CopilotChat" },
+        {
+          "<leader>cc",
+          cc.toggle,
+          desc = "Toggle"
+        },
+        {
+          "<leader>cq",
+          function()
+            local input = vim.fn.input("Quick Chat: ")
+            if input ~= "" then
+              cc.ask(input, { selection = require("CopilotChat.select").buffer })
+            end
+          end,
+          desc = "Quick chat"
+        },
+        {
+          "<leader>cp",
+          function()
+            local actions = require("CopilotChat.actions")
+            require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+          end,
+          desc = "Prompt actions"
+        },
+      })
+    end,
   },
 }
