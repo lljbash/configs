212623dd3f1693ef273bc0e5dce91693797f1a2f
lljbash <lljbash@gmail.com>
2025-05-13 09:13:57 +0000

    feat(nvim): revert sonokai diagnostic text highlight

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -10,7 +10,7 @@ return {
         let g:sonokai_style = "shusia"
         let g:sonokai_disable_italic_comment = 0
         let g:sonokai_enable_italic = 1
-        let g:sonokai_diagnostic_text_highlight = 1
+        " let g:sonokai_diagnostic_text_highlight = 1
         let g:sonokai_diagnostic_virtual_text = "colored"
         colorscheme sonokai
       ]])
