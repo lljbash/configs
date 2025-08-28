8137c516c48e78a6c329cceffa481cde879542e9
lljbash <lljbash@gmail.com>
2025-08-28 11:40:22 +0000

    fix(nvim): keep up with latest auto-session changes

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,25 +1,25 @@
 return {
   {
     "rmagatti/auto-session",
+    lazy = false,
+    keys = {
+      { "<C-s>", "<cmd>AutoSession search<CR>", desc = "Session search" },
+    },
     init = function()
       vim.o.sessionoptions =
         "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
     end,
-    config = function()
-      require("auto-session").setup({
-        post_restore_cmds = {
-          function()
-            for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-              if vim.bo[bufnr].buftype == "" then
-                vim.fn.bufload(bufnr)
-              end
+    opts = {
+      post_restore_cmds = {
+        function()
+          for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.bo[bufnr].buftype == "" then
+              vim.fn.bufload(bufnr)
             end
-          end,
-        },
-      })
-      vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
-        noremap = true,
-      })
-    end,
+          end
+        end,
+      },
+      load_on_setup = true,
+    },
   },
 }
