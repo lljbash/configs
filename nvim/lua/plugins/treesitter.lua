1c891402748c06929ddb89e9e4ae6bdbd1d09f64
lljbash <lljbash@gmail.com>
2026-01-04 17:59:35 +0800

    fix(nvim): keep up with latest treesitter:main

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      { "LiadOz/nvim-dap-repl-highlights", opts = {} }
+      { "LiadOz/nvim-dap-repl-highlights", opts = {} },
     },
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
+    main = "nvim-treesitter.config",
     opts = {
       ensure_installed = {
         "bash",
@@ -32,11 +32,20 @@ return {
       vim.opt.foldmethod = "expr"
       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
       vim.opt.foldlevel = 99 -- Open all folds by default
-    end
+    end,
   },
 
   -- Debug treesitter
+  --
+  -- ## Deprecation notice
+  --
+  -- This plugin is **deprecated** since the functionality is included in Neovim: Use
+  --
+  -- - `:Inspect` to show the highlight groups under the cursor
+  -- - `:InspectTree` to show the parsed syntax tree ("TSPlayground")
+  -- - `:EditQuery` to open the Live Query Editor (Nvim 0.10+)
   {
+    enabled = false,
     "nvim-treesitter/playground",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
@@ -50,7 +59,7 @@ return {
       highlight = "LspInlayHint",
       min_rows_ft = {
         python = 50,
-      }
+      },
     },
-  }
+  },
 }
