71ba83151e00751059eb47213e0ffef7b6e1906c
lljbash <lljbash@gmail.com>
2026-01-04 18:12:12 +0800

    fix(nvim): use master branch of telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
+    branch = "master",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
